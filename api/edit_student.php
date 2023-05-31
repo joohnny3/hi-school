1d225cccecaddfa4b98887ba68efccbc3eea58a7
joohnny3 <johnny31258@gmail.com>
2023-05-31 13:45:44 +0800

    前台學生修改基本資料功能增加

--- api/edit_student.php
+++ api/edit_student.php
@@ -1,9 +1,26 @@
 <?php
-
+include_once "../db.php";
 
 print "<pre>";
 print_r($_POST);
 print "</pre>";
 
 
-?>
+$sql = "update `students` 
+     set  
+      `en_name`='{$_POST['en_name']}',
+      `birthday`='{$_POST['birthday']}',
+      `uni_id`='{$_POST['uni_id']}',
+      `addr`='{$_POST['addr']}',
+      `tel`='{$_POST['tel']}',
+      `dept`='{$_POST['dept']}',
+      `email`='{$_POST['email']}',
+      `guardian`='{$_POST['guardian']}'
+       where `school_num`='{$_POST['school_num']}'";
+
+$pdo->exec($sql);   
+    
+    
+    
+
+header("location:../index.php?do=student");
