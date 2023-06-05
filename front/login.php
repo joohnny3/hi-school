58e059bef319510ff412c7ad8ca69f607ce784fd
joohnny3 <johnny31258@gmail.com>
2023-06-05 23:34:53 +0800

    重新匯出資料庫

--- front/login.php
+++ front/login.php
@@ -6,10 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
+    
 </head>
 
 <body>
-    <h1>login</h1>
     <?php
     if (isset($_GET['error'])) {
         echo "<span style='color:red'>";
--- index.php
+++ index.php
@@ -6,32 +6,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>好學校</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Roboto&display=swap" rel="stylesheet">
+    <title>hi_School</title>
     <style>
         body {
             margin: 0;
             padding: 0;
         }
+
+        a {
+            font-family: 'Roboto', sans-serif;
+            font-size: 44px;
+        }
+
+        h1 {
+            font-family: 'Dancing Script', cursive;
+            font-size: 108px;
+            text-align: center;
+        }
+
+        .menu {
+            display: flex;
+            justify-content: space-around;
+            background: #f1f1f1;
+            padding: 20px 0;
+        }
+
+        .menu-item {
+            cursor: pointer;
+        }
     </style>
 </head>
 
 <body>
+    <h1>hi_School</h1>
     <header>
         <?php
         if (!isset($_SESSION['login'])) {
         ?>
-            <a href="index.php?do=login">登入</a>
-            <a href="index.php?do=reg">註冊</a>
+        <div class="menu">
+
+            <div class="menu-item" id="login">
+                <a href="index.php?do=login">Log in</a>
+            </div>
+            <div class="menu-item" id="register">
+                <a href="index.php?do=reg">Sing up</a>
+            </div>
+        </div>
         <?php } ?>
     </header>
     <main>
-        <?php
-        $showSidebar = true;
-        $do = $_GET['do'] ?? '';
-        $file = "./front/" . $do . ".php";
-        
-        include file_exists($file) ? $file : "";
-        ?>
+        <div id="content">
+            <?php
+            $showSidebar = true;
+            $do = $_GET['do'] ?? '';
+            $file = "./front/" . $do . ".php";
+
+            if (file_exists($file)) {
+                include $file;
+            }
+            ?>
+        </div>
     </main>
     <footer></footer>
 </body>
